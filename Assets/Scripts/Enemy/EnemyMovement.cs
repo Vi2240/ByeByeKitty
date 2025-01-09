4aed8fdd9a275f83d207b9aaf4fb0dd08ae0417e
WWalter <146815290+06Lizard@users.noreply.github.com>
2025-01-09 15:16:38 +0100

    Revert "Merge branch 'main' of https://github.com/Vi2240/ByeByeKitty into Vincent"
    This reverts commit 520bff07cf69b2e4568f4c2ae8645ced782c279d, reversing
    changes made to 13e86fcc2ffaa07b648eef906168abf1c5823435.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -92,8 +92,6 @@ public class EnemyMovement : MonoBehaviour
         nearestPlayer = FindNearestObject("Player");
         nearestObjective = FindNearestObject("Objective");
 
-        NearObjectChecks();
-
         CheckIfStuck();
 
         if (BehaviourChecks()) { return; };
@@ -182,10 +180,15 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    void NearObjectChecks()
+    private void OnTriggerEnter2D(Collider2D trigger)
     {
-        if (DistanceTo(nearestPlayer) <= nearObjectCheckRadius) { isNearPlayer = true; }
-        else
+        if (trigger.CompareTag("Player")) { isNearPlayer = true; }
+        if (trigger.CompareTag("Objective")) { isNearObjective = true; }
+    }
+
+    private void OnTriggerExit2D(Collider2D trigger)
+    {
+        if (trigger.CompareTag("Player"))
         {
             isNearPlayer = false;
             if (targetRestriction != 1) // Find objective if not restricted to only players.
@@ -322,7 +325,7 @@ public class EnemyMovement : MonoBehaviour
         canMove = true;
     }
 
-    public float DistanceTo(GameObject _object) // Public so it can be used in other scripts
+    public float DistanceTo(GameObject _object)
     {
         return Vector2.Distance(gameObject.transform.position, _object.transform.position);
     }
