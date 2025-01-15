023dfaae2c303ffafd68462368af787e9fd93bfa
Vi2240 <143485216+Vi2240@users.noreply.github.com>
2025-01-15 14:56:57 +0100

    changes

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,17 +1,13 @@
 using NUnit.Framework.Internal;
-using System;
 using System.Collections;
-using UnityEditor;
+using System;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
 
     [SerializeField] GameObject normalEnemy, speedEnemy, slowEnemy, rangeEnemy;
-    [SerializeField] Collider2D[] colliders; // save all colliders in the are that enemy should not spawn on
-    [SerializeField] LayerMask notSpawnLayer; // de layer that it does not have selekten enemys can spawn on
-
-    [SerializeField] float radiusOfArea;
+    [SerializeField] GameObject[] spawnPoints;
 
     [Header("ChanceToSpawnEnemy")]
     [SerializeField] float NormalEnemy_ChanceToSpawn = 95f;
@@ -19,31 +15,15 @@ public class WaveSpawner : MonoBehaviour
     [SerializeField] float SlowEnemy_ChanceToSpawn = 1f;
     [SerializeField] float RangeEnemy_ChanceToSpawn = 0.5f;
 
-    [Header("SpawnRange")]
-    [SerializeField] float minSpawnPosX = -5f;
-    [SerializeField] float maxSpawnPosX = 5f;
-    [SerializeField] float minSpawnPosY = -5f;
-    [SerializeField] float maxSpawnPosY = 5f;
-
-    [Header("EnemyCount")]
-    [SerializeField] int maxEnemyCountPerWave;
-    [SerializeField] int maxEnemyCount;
-    [SerializeField] int enemyCountPerWave;
-    [SerializeField] int enemyCount;
-
     [Header("Timers")]
-    [SerializeField] float enemyFrequencyTime;
-    [SerializeField] float waveTime = 30f;
-    [SerializeField] float waveDelay = 10f;
+    [SerializeField] float spawnRate;
 
     Objective objective;
 
-    bool canSpawn;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-
+        objective = GetComponent<Objective>();
+        StartCoroutine(Spawner());
     }
 
     GameObject RandomizeEnemyToSpawn()
@@ -80,95 +60,17 @@ public class WaveSpawner : MonoBehaviour
         return null;
     }
 
-    void EnemySpawnPos(GameObject _enemyToSpawn)
+    IEnumerator Spawner()
     {
-        Vector2 spawnPos = Vector2.zero;
-        bool canSpawnHere = false;
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
-        while (!canSpawnHere)
+        while (objective.CanSpawn)
         {
-            float spawnPosX = UnityEngine.Random.Range(minSpawnPosX, maxSpawnPosX);
-            float spawnPosY = UnityEngine.Random.Range(minSpawnPosY, maxSpawnPosY);
+            yield return wait;
+            GameObject enemyToSpawn = RandomizeEnemyToSpawn();
+            int randomSpawnPoint = UnityEngine.Random.Range(0, spawnPoints.Length);
 
-            spawnPos = new Vector2(spawnPosX, spawnPosY);
-            canSpawnHere = PrentsSpawnOverlap(spawnPos);
-            canSpawn = canSpawnHere;
-
-            if (canSpawnHere)
-            {
-                break;
-            }
+            Instantiate(enemyToSpawn, spawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
         }
-
-
-        GameObject enemyToSpawn = _enemyToSpawn;
-        SpawnEnemy(enemyToSpawn, spawnPos);
-    }
-
-    bool PrentsSpawnOverlap(Vector2 spawnPos)
-    {
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            Vector3 centerPoint = colliders[i].bounds.center;
-
-            float width = colliders[i].bounds.extents.x;
-            float height = colliders[i].bounds.extents.y;
-
-            float leftExtent = centerPoint.x - width;
-            float rightExtent = centerPoint.x + width;
-            float lowerExtent = centerPoint.y - height;
-            float upperExtent = centerPoint.y + height;
-
-            if (spawnPos.x >= leftExtent && spawnPos.x <= rightExtent)
-            {
-                if (spawnPos.y >= lowerExtent && spawnPos.y <= upperExtent)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        return true;
-    }
-
-    void SpawnEnemy(GameObject enemyToSpawn, Vector2 spawnPos)
-    {
-        if (canSpawn && enemyToSpawn != null)
-        {
-            GameObject newEnemy = Instantiate(enemyToSpawn, spawnPos, Quaternion.identity);
-            enemyCountPerWave++;
-        }
-    }
-
-    public void AddEnemyCount()
-    {
-        enemyCount++;
-    }
-
-    public void SubEnemyCount()
-    {
-        enemyCount--;
-    }
-
-    public IEnumerator Waves()
-    {
-        while (enemyCountPerWave < maxEnemyCountPerWave && enemyCount < maxEnemyCount)
-        {
-            if (enemyCountPerWave >= maxEnemyCountPerWave && enemyCount >= maxEnemyCount)
-            {
-                break;
-            }
-
-            yield return new WaitForSeconds(enemyFrequencyTime);
-
-            EnemySpawnPos(RandomizeEnemyToSpawn());
-        }
-
-        yield return new WaitForSeconds(waveTime);
-
-        StartCoroutine(Waves());
-        enemyCountPerWave = 0;
     }
 }
